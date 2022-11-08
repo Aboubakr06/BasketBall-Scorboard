3c432acb4cab94d021a28b30864d6b41843fe1ae
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2022-11-08 21:37:43 +0100

    co

---
+++ script.js
@@ -0,0 +1,35 @@
+let scoreh = 0;
+let Hscore = document.getElementById("h-score");
+
+function hadd1(){
+    scoreh = scoreh + 1
+    Hscore.textContent = scoreh
+}
+
+function hadd2(){
+    scoreh = scoreh + 2
+    Hscore.textContent = scoreh
+}
+
+function hadd3(){
+    scoreh = scoreh + 3
+    Hscore.textContent = scoreh
+}
+
+let scoreg = 0;
+let Gscore = document.getElementById("g-score");
+
+function gadd1(){
+    scoreg = scoreg + 1
+    Gscore.textContent = scoreg
+}
+
+function gadd2(){
+    scoreg = scoreg + 2
+    Gscore.textContent = scoreg
+}
+
+function gadd3(){
+    scoreg = scoreg + 3
+    Gscore.textContent = scoreg
+}
